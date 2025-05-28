15b8eafc574cef6b3ca4040c624ebfb9e6e643cb
Левченко Евгений Викторович (4044064) <elevchenko@dev.vtb.ru>
2025-05-28 22:55:15 +0200

    day1 second version

--- src/main/java/strings/easy/IsomorphicStrings.java
+++ src/main/java/strings/easy/IsomorphicStrings.java
@@ -21,12 +21,17 @@ public class IsomorphicStrings {
         return prefix;
     }
 
-
-    public static void main(String[] args) {
-        String[] test = {"abcf", "abct", "abcf"};
-        var prefix = longestCommonPrefix(test);
-        System.out.printf(prefix);
+    public static String longestCommonPrefixFirst(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
     }
-
-
 }
